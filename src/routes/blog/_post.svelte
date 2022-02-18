154daf85a9aa453ba46bab3b15f8a57e2de06fe4
Johannes Herrnegger <johannes.herrnegger@gmail.com>
2022-02-18 08:42:02 +0100

    use patch-package instead of git repository

--- src/routes/blog/_post.svelte
+++ src/routes/blog/_post.svelte
@@ -4,21 +4,18 @@
 	export let title;
 	export let date;
 	export let description;
-    export let readingTime;
-    // export let post;
+	export let readingTime;
+	// export let post;
 	// export let timeToRead = Math.ceil(readingTime(post.body).minutes);
 	const metadescription = description;
 </script>
 
-<SEO
-	{title}
-	{metadescription}
-    isArticle={true}
-    publishedTime={date}
-/>
+<SEO {title} {metadescription} isArticle={true} publishedTime={date} />
 
 <div class="prose dark:prose-dark mx-auto">
-	<h1>{title}</h1>
-	<p>Published: {date}, Reading time: {readingTime.text}</p>
+	<h1 class="mb-0">{title}</h1>
+	<p class="text-sm">
+		{readingTime.text}, Published on {new Date(date).toLocaleDateString('en-UK')}
+	</p>
 	<slot />
 </div>
