721ead4aede11b905165ddda855c9b30561e525e
Johannes Herrnegger <johannes.herrnegger@gmail.com>
2022-02-26 08:42:45 +0100

    Hightlight active route in navigation

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,4 +1,7 @@
 <script>
+    import { page } from '$app/stores';
+    let path, currentActiveUrl;
+
     const navItems = [
         {
             url: '/',
@@ -16,7 +19,19 @@
             url: '/about',
             label: 'About',
         },
-    ]
+    ];
+
+    $: {
+        path = $page.url.pathname;
+        currentActiveUrl = '/';
+        if(path !== '/') {
+            const currentActiveRoute = navItems.find(item =>
+                item.url !== '/'
+                && path.startsWith(item.url)
+            );
+            currentActiveUrl = currentActiveRoute.url;
+        }
+    }
 </script>
 
 <header class="container max-w-prose flex flex-row justify-between py-5 mb-10 border-b-2">
@@ -24,7 +39,7 @@
 	<nav>
 		<ul class="flex flex-row">
             {#each navItems as item}
-                <li><a href="{item.url}" class="px-1 py-1 mx-1">{item.label}</a></li>
+                <li><a href="{item.url}" class="px-1 py-1 mx-1 {item.url === currentActiveUrl ? 'font-bold' : ''}">{item.label}</a></li>
             {/each}
 		</ul>
 	</nav>
